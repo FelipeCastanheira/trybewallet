32a054aa37ac46acaf5a55201dd669b25449dd8d
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-02 18:54:05 -0300

    addExpense reducer

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,12 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = { currencies: [], expenses: [] };
+
+function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'ADD_EXPENSE':
+    return { ...state, expenses: [...state.expenses, action.value] };
+  default:
+    return state;
+  }
+}
+
+export default wallet;
