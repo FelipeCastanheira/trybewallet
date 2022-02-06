9daf204973d9bf87c533764fc1fd9b1f5091cc0f
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-06 19:53:19 -0300

    update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import logoWallet from '../assets/logoWallet.png';
+import calculateExpenses from '../functions';
 
 class Header extends React.Component {
   render() {
-    const { userLogin } = this.props;
+    const { userLogin, expenseData } = this.props;
+    const total = calculateExpenses(expenseData.expenses);
     return (
       <header>
         <img src={ logoWallet } alt="logo trybe wallet" />
@@ -18,7 +20,7 @@ class Header extends React.Component {
           </p>
           <p>
             <span>Despesa Total: R$</span>
-            <span data-testid="total-field">0</span>
+            <span data-testid="total-field">{total}</span>
             <span data-testid="header-currency-field">BRL</span>
           </p>
         </h4>
@@ -29,6 +31,16 @@ class Header extends React.Component {
 
 Header.propTypes = {
   userLogin: PropTypes.objectOf(PropTypes.string).isRequired,
+  expenseData: PropTypes.shape({
+    expenses: PropTypes.arrayOf(PropTypes.shape({
+      value: PropTypes.string,
+      currency: PropTypes.string,
+      method: PropTypes.string,
+      tag: PropTypes.string,
+      description: PropTypes.string,
+      id: PropTypes.string,
+    })).isRequired,
+  }).isRequired,
 };
 
 const mapStateToProps = (state) => ({
