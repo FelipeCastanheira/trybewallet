3502992bc8844c258d02ed0cc53f21e658ca2ca1
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-06 19:52:03 -0300

    remove unused action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 export const addExpense = (value) => ({ type: 'ADD_EXPENSE', value });
 export const login = (value) => ({ type: 'LOGIN', value });
-export const getCurrencies = (value) => ({ type: 'CURRENCY', value });
+export const getCurrencies = (value) => ({ type: 'CURRENCIES', value });
 export const requestAPI = () => ({ type: 'REQUEST_API' });
 export const removeAction = (index) => ({ type: 'REMOVE', index });
 
@@ -10,8 +10,11 @@ export function fetchAPI(expense) {
       dispatch(requestAPI());
       const response = await fetch('https://economia.awesomeapi.com.br/json/all');
       const data = await response.json();
-      dispatch(getCurrencies(data));
-      dispatch(addExpense(expense));
+      if (expense) {
+        dispatch(addExpense(expense));
+      } else {
+        dispatch(getCurrencies(data));
+      }
     } catch (error) {
       console.error(error);
     }
