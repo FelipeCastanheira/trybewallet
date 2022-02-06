370dec3b3f73a595dc3ce9eb8f15a42179b95fa8
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-06 19:52:36 -0300

    auxiliar function

---
+++ src/functions/index.js
@@ -0,0 +1,11 @@
+const calculateExpenses = (expensesArray) => {
+  const result = expensesArray.reduce((sum, element) => {
+    const { value, currency, exchangeRates } = element;
+    const exchangeValue = exchangeRates[currency];
+    const total = sum + (value * exchangeValue);
+    return total;
+  }, 0);
+  return result.toFixed(2);
+};
+
+export default calculateExpenses;
