d4883cd786f3c4e282ffbd65f223405bd21f6992
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-06 18:14:13 -0300

    reducer with thunk

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,9 +1,27 @@
-const INITIAL_STATE = { currencies: [], expenses: [] };
+const INITIAL_STATE = { currencies: [], expenses: [], isFetching: false };
+
+const updateLastExpense = (array, value) => {
+  array.forEach((element, index) => {
+    if (index === (array.length - 1)) element.exchangeRates = value;
+  });
+  return array;
+};
 
 function wallet(state = INITIAL_STATE, action) {
   switch (action.type) {
+  case 'REQUEST_API':
+    return { ...state, isFetching: true };
+  case 'CURRENCY':
+    return { ...state,
+      currencies: action.value,
+      expenses: updateLastExpense(state.expenses, action.value),
+      isFetching: false };
   case 'ADD_EXPENSE':
     return { ...state, expenses: [...state.expenses, action.value] };
+  case 'REMOVE':
+    return { ...state,
+      expenses: state.expenses.filter((_data, i) => i !== action.index),
+    };
   default:
     return state;
   }
