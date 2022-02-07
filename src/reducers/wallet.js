f24fd96964cb9a43e705c3d5e40fc5ab99b233ac
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-07 20:15:52 -0300

    editExpense reducer

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,23 +1,22 @@
-const INITIAL_STATE = { currencies: {}, expenses: [], isFetching: false };
-
-// const updateLastExpense = (array, value) => {
-//   array.forEach((element, index) => {
-//     if (index === (array.length - 1)) element.exchangeRates = value;
-//   });
-//   return array;
-// };
+const INITIAL_STATE = { currencies: {},
+  expenses: [],
+  isFetching: false,
+  editData: { id: 0, exchangeRates: { USD: '' }, isUpdating: false } };
 
 function wallet(state = INITIAL_STATE, action) {
   switch (action.type) {
   case 'REQUEST_API':
     return { ...state, isFetching: true };
+  case 'EDIT':
+    return { ...state, editData: action.value };
   case 'CURRENCIES':
     return { ...state, currencies: action.value, isFetching: false };
   case 'ADD_EXPENSE':
     return { ...state, expenses: [...state.expenses, action.value], isFetching: false };
   case 'REMOVE':
     return { ...state,
-      expenses: state.expenses.filter((_data, i) => i !== action.index),
+      expenses: state.expenses.filter(({ id }) => id !== action.index),
+      editData: { ...state.editData, isUpdating: false },
     };
   default:
     return state;
